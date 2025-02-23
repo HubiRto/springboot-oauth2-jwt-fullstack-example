4d352dbfcc879567eb18bfcf5d297615857fbfb0
hubirto <hubert.rybicki.hr@gmail.com>
2025-02-23 22:18:30 +0100

    Connect auth filter and unauthorized entry point to security filter chain

--- backend/src/main/java/eu/pomoku/backend/configs/SecurityConfig.java
+++ backend/src/main/java/eu/pomoku/backend/configs/SecurityConfig.java
@@ -1,22 +1,45 @@
 package eu.pomoku.backend.configs;
 
+import eu.pomoku.backend.security.AuthEntryPoint;
+import eu.pomoku.backend.security.AuthTokenFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthTokenFilter authTokenFilter;
+    private final AuthEntryPoint authEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
+        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling((exception) -> exception.authenticationEntryPoint(authEntryPoint));
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
